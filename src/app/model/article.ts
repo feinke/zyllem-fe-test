0718e6974c47e68512a3363626127256c32da89e
feinke <feinke@gmail.com>
2020-01-09 21:16:11 +0800

    first commit

---
+++ src/app/model/article.ts
@@ -0,0 +1,21 @@
+
+export abstract class Article {
+    title: string;
+    author: string;
+    publishedAt: string; //date time in ISO format 
+    url: string;
+    type: ArticleType;
+}
+
+export class NormalArticle extends Article {
+    description: string;
+}
+
+export class FeaturedArticle extends  Article{
+    featureImgUrl: string;
+}
+
+export enum ArticleType {
+    NORMAL = "NORMAL",
+    FEATURED = "FEATURED",
+}
